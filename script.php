aec3c66f22897faba7a674cc072300fde1b0103b
Mamadou NDIAYE <mamadou.ndiaye.ext@abtasty.com>
2024-09-18 11:35:04 +0200

    FIRST COMMIT

---
+++ script.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @param $nums array
+ * @param $target number
+ * @return array
+ */
+    function twoSum($nums, $target)
+    {
+        return [];
+    }
+
+    $nums = [2, 7, 11, 15];
+    $target = 17;
+    $result = twoSum($nums, $target);
+    print_r($result);
